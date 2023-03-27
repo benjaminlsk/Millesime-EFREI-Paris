eece2595afee2310cec3b70a13b691df51ae0204
benjaminlsk <73712469+benjaminlsk@users.noreply.github.com>
2023-03-27 19:45:27 +0200

    fix changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,6 @@ const association = [
     icon: CursorArrowRaysIcon,
   },
 ];
-const callsToAction = [{ name: "Contact", href: "#", icon: PhoneIcon }];
 const events = [
   {
     name: "Events",
@@ -120,22 +119,6 @@ export default function Header() {
                             </a>
                           ))}
                         </div>
-                        <div className="space-y-6 bg-#800020 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                          {callsToAction.map((item) => (
-                            <div key={item.name} className="flow-root">
-                              <a
-                                href={item.href}
-                                className="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100"
-                              >
-                                <item.icon
-                                  className="h-6 w-6 flex-shrink-0 text-gray-400"
-                                  aria-hidden="true"
-                                />
-                                <span className="ml-3">{item.name}</span>
-                              </a>
-                            </div>
-                          ))}
-                        </div>
                       </div>
                     </Popover.Panel>
                   </Transition>
@@ -227,37 +210,6 @@ export default function Header() {
                         </div>
                       </div>
                     </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
-            <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className={classNames(
-                      open ? "text-gray-900" : "text-gray-500",
-                      "group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-offset-2"
-                    )}
-                  >
-                    <span>Contact</span>
-                    <ChevronDownIcon
-                      className={classNames(
-                        open ? "text-gray-600" : "text-gray-400",
-                        "ml-2 h-5 w-5 group-hover:text-gray-500"
-                      )}
-                      aria-hidden="true"
-                    />
-                  </Popover.Button>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >               
                   </Transition>
                 </>
               )}
